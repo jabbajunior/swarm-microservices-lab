780849103c047ec1c7e51da4b8f3b3839e8252c0
jabbajunior <keiran.singh@gmail.com>
2026-06-11 20:35:53 +0000

    feat(ordering): add order update schema

--- app/ordering_service/schemas.py
+++ app/ordering_service/schemas.py
@@ -26,3 +26,9 @@ class OrderResponse(OrderBase):
     total_price: Decimal = Field(gt=0, max_digits=12, decimal_places=2)
     status: OrderStatus = Field(default=OrderStatus.PENDING)
     user_id: int | None
+
+
+# Purposefully Omitting product_id as the logic can get quite messy
+class OrderUpdate(BaseModel):
+    quantity: int | None = Field(default=None, gt=0)
+    status: OrderStatus | None = Field(default=None)
