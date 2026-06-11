4ee3b87aca701a6ff132e1a2c1dc4e2220dba7ae
jabbajunior <keiran.singh@gmail.com>
2026-06-11 20:32:12 +0000

    feat(ordering): add order schemas

---
+++ app/ordering_service/schemas.py
@@ -0,0 +1,28 @@
+from decimal import Decimal
+
+from pydantic import BaseModel, ConfigDict, Field
+
+from app.ordering_service.enums import OrderStatus
+
+
+class OrderBase(BaseModel):
+    product_id: int = Field(gt=0)
+    quantity: int = Field(gt=0)
+
+
+# Order - User supplies to Create
+class OrderCreate(OrderBase):
+    pass
+
+
+# Order Responses
+class OrderResponse(OrderBase):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    product_id: int
+    quantity: int
+
+    total_price: Decimal = Field(gt=0, max_digits=12, decimal_places=2)
+    status: OrderStatus = Field(default=OrderStatus.PENDING)
+    user_id: int | None
