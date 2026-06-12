bfa0c5ea9180e73ce7578997c883f00b6bbb653f
jabbajunior <keiran.singh@gmail.com>
2026-06-12 16:06:33 +0000

    feat(user): add user database model

---
+++ app/auth_service/models.py
@@ -0,0 +1,21 @@
+# SQLAlchemy (Database) Models for user service
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.database import Base
+
+# Nullable explicitly for important business fields
+# Can imply nullable via typehints with [type | None]
+
+
+# User Table
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        Integer, primary_key=True, index=True, unique=True
+    )
+    username: Mapped[str] = mapped_column(String(64), nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(254), nullable=False)
